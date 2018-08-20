04c37c3fdde704365c37b59c5787f3e9dfc9086f
Sari Sabban <sari.sabban@gmail.com>
2018-08-20 18:55:07 +0300

    Update Rg.py

--- Rg.py
+++ Rg.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-import math , sys
+import math 
+import sys
 
 def Rg(filename):
-	''' Calculates the Radius of Gyration (Rg) of a protein given its .pdb structure file (adapted from the PyMol script https://pymolwiki.org/index.php/Radius_of_gyration) '''
-	''' Returs the Rg integer value '''
+	'''
+	Calculates the Radius of Gyration (Rg) of a protein given its .pdb 
+	structure file. Returns the Rg integer value in Angstrom.
+	(adapted from https://pymolwiki.org/index.php/Radius_of_gyration)
+	'''
 	coord = list()
 	mass = list()
-	Structure = open(filename , 'r')
+	Structure = open(filename, 'r')
 	for line in Structure:
 		try:
 			line = line.split()
 			x = float(line[6])
 			y = float(line[7])
 			z = float(line[8])
-			coord.append([x , y , z])
+			coord.append([x, y, z])
 			if line[-1] == 'C':
 				mass.append(12.0107)
 			elif line[-1] == 'O':
@@ -25,11 +29,12 @@ def Rg(filename):
 				mass.append(32.065)
 		except:
 			pass
-	xm = [(m * i , m * j , m * k) for (i , j , k) , m in zip(coord , mass)]
+	xm = [(m*i, m*j, m*k) for (i, j, k), m in zip(coord, mass)]
 	tmass = sum(mass)
-	rr = sum(mi * i + mj * j + mk * k for (i , j , k) , (mi , mj , mk) in zip(coord , xm))
-	mm = sum((sum(i) / tmass) ** 2 for i in zip( * xm))
-	rg = math.sqrt(rr / tmass - mm)
-	return(rg)
+	rr = sum(mi*i + mj*j + mk*k for (i, j, k), (mi, mj, mk) in zip(coord, xm))
+	mm = sum((sum(i) / tmass)**2 for i in zip(*xm))
+	rg = math.sqrt(rr / tmass-mm)
+	return(round(rg, 3))
 
-print(Rg(sys.argv[1]))
+if __name__ == '__main__':
+	print('Rg = {}'.format(Rg(sys.argv[1])))
